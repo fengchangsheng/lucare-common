ba10741ba7be8290d45dd096feee4a73b7782102
fcs <fengchangsheng123@qq.com>
2016-09-01 22:55:14 +0800

    update

--- src/main/java/com/fcs/common/config/jfinal/Prop.java
+++ src/main/java/com/fcs/common/config/jfinal/Prop.java
@@ -10,22 +10,21 @@ import java.util.Properties;
  */
 public class Prop {
 
-    private Properties properties;
+    private Properties properties = null;
 
     public Prop(String fileName) {
         this(fileName,"UTF-8");
     }
 
     public Prop(String fileName, String encoding) {
-        this.properties = null;
         InputStream inputStream = null;
         try {
             inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
             if (inputStream == null) {
                 throw new IllegalArgumentException("Properties file not found in classpath: " + fileName);
             }
-            this.properties = new Properties();
-            this.properties.load(new InputStreamReader(inputStream,encoding));
+            properties = new Properties();
+            properties.load(new InputStreamReader(inputStream,encoding));
         } catch (IOException e) {
             throw new RuntimeException("Error loading properties file.", e);
         } finally {
@@ -45,5 +44,56 @@ public class Prop {
 
     public String get(String key, String defaultValue) {
         return this.properties.getProperty(key, defaultValue);
+    }
+
+    public Integer getInt(String key) {
+        return getInt(key, null);
+    }
+
+    public Integer getInt(String key, Integer defaultValue) {
+        String value = properties.getProperty(key);
+        if (value != null) {
+            return Integer.parseInt(value.trim());
+        }
+        return defaultValue;
+    }
+
+    public Long getLong(String key) {
+        return getLong(key, null);
+    }
+
+    public Long getLong(String key, Long defaultValue) {
+        String value = properties.getProperty(key);
+        if (value != null) {
+            return Long.parseLong(value.trim());
+        }
+        return defaultValue;
+    }
+
+    public Boolean getBoolean(String key) {
+        return getBoolean(key, null);
+    }
+
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        String value = properties.getProperty(key);
+        if (value != null) {
+            value = value.toLowerCase().trim();
+            if ("true".equals(value)) {
+                return true;
+            } else if ("false".equals(value)) {
+                return false;
+            } else {
+                throw new RuntimeException("The value can not parse to Boolean : " + value);
+            }
+        }
+        return defaultValue;
+    }
+
+    public boolean containsKey(String key) {
+        return properties.containsKey(key);
+    }
+
+    public Properties getProperties() {
+        return properties;
     }
 }
--- src/main/java/com/fcs/common/config/jfinal/PropKit.java
+++ src/main/java/com/fcs/common/config/jfinal/PropKit.java
@@ -27,4 +27,64 @@ public class PropKit {
         }
         return result;
     }
+
+    public static Prop useless(String fileName) {
+        Prop previous = map.remove(fileName);
+        if (PropKit.prop == previous) {
+            PropKit.prop = null;
+        }
+        return previous;
+    }
+
+    public static void clear() {
+        prop = null;
+        map.clear();
+    }
+
+    public static Prop getProp() {
+        if (prop == null) {
+            throw new IllegalStateException("Load propties file by invoking Propkit.use(String fileName) method first.");
+        }
+        return prop;
+    }
+
+    public static Prop getProp(String fileName) {
+        return map.get(fileName);
+    }
+
+    public static String get(String key) {
+        return getProp().get(key);
+    }
+
+    public static String get(String key, String defaultValue) {
+        return getProp().get(key, defaultValue);
+    }
+
+    public static Integer getInt(String key) {
+        return getProp().getInt(key);
+    }
+
+    public static Integer getInt(String key, Integer defaultValue) {
+        return getProp().getInt(key, defaultValue);
+    }
+
+    public static Long getLong(String key) {
+        return getProp().getLong(key);
+    }
+
+    public static Long getLong(String key, Long defaultValue) {
+        return getProp().getLong(key, defaultValue);
+    }
+
+    public static Boolean getBoolean(String key) {
+        return getProp().getBoolean(key);
+    }
+
+    public static Boolean getBoolean(String key, Boolean defaultValue) {
+        return getProp().getBoolean(key, defaultValue);
+    }
+
+    public static boolean containsKey(String key) {
+        return getProp().containsKey(key);
+    }
 }
